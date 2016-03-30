ece6c047d91828d1a7ac27a14aa8efb34b7e57fd
François Lecomte <francois.lecomte@gmail.com>
2016-03-30 21:47:09 +0200

    initial commit

---
+++ src/main/java/com/github/lothar/security/acl/SpringSecurityAclFeaturesApplication.java
@@ -0,0 +1,12 @@
+package com.github.lothar.security.acl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAclFeaturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAclFeaturesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/github/lothar/security/acl/SpringSecurityAclFeaturesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.lothar.security.acl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringSecurityAclFeaturesApplication.class)
+public class SpringSecurityAclFeaturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
