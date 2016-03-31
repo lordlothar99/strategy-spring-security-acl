322944a5642ac4f5c300b9e0e4ab74d0c12793b3
François Lecomte <francois.lecomte@gmail.com>
2016-03-31 23:56:10 +0200

    Jpa Specification feature #9 ; DenyAllSpecification

---
+++ jpa/src/main/java/com/github/lothar/security/acl/jpa/spec/DenyAllSpecification.java
@@ -0,0 +1,21 @@
+package com.github.lothar.security.acl.jpa.spec;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class DenyAllSpecification<T> implements Specification<T> {
+
+  @Override
+  public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+    return cb.disjunction();
+  }
+
+  @Override
+  public String toString() {
+    return getClass().getSimpleName();
+  }
+}
