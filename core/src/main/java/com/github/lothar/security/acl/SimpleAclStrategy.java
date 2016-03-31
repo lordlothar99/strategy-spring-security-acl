ce2b9ed6608933975596c2962dcf49fdff852f88
François Lecomte <francois.lecomte@gmail.com>
2016-03-31 21:49:35 +0200

    beanName aware

--- core/src/main/java/com/github/lothar/security/acl/SimpleAclStrategy.java
+++ core/src/main/java/com/github/lothar/security/acl/SimpleAclStrategy.java
@@ -4,10 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import org.springframework.beans.factory.BeanNameAware;
+
 @SuppressWarnings("unchecked")
-public class SimpleAclStrategy implements AclStrategy {
+public class SimpleAclStrategy implements AclStrategy, BeanNameAware {
 
   private Map<AclFeatureType<?>, Object> features = new HashMap<>();
+  private String name;
 
   public <Feature> void register(AclFeatureType<Feature> featureType, Feature feature) {
     features.put(featureType, feature);
@@ -22,7 +25,16 @@ public class SimpleAclStrategy implements AclStrategy {
   }
 
   @Override
+  public void setBeanName(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  @Override
   public String toString() {
-    return Objects.toString(features);
+    return Objects.toString(name, getClass().getName()) + ":" + Objects.toString(features);
   }
 }
