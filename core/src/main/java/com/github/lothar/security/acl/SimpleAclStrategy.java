e2a633a798a2fb0a00b37e4717a45d570bdea07b
François Lecomte <francois.lecomte@gmail.com>
2016-03-31 18:41:31 +0200

    support for feature subclasses

--- core/src/main/java/com/github/lothar/security/acl/SimpleAclStrategy.java
+++ core/src/main/java/com/github/lothar/security/acl/SimpleAclStrategy.java
@@ -7,8 +7,12 @@ public class SimpleAclStrategy implements AclStrategy {
 
   private Map<AclFeatureType<?>, Object> features = new HashMap<>();
 
-  public <Feature> void register(AclFeatureType<Feature> featureType, Feature feature) {
+  public <Feature, F extends Feature> void register(AclFeatureType<Feature> featureType, F feature) {
     features.put(featureType, feature);
+  }
+
+  public <Feature> void unregister(AclFeatureType<Feature> featureType) {
+    features.remove(featureType);
   }
 
   @SuppressWarnings("unchecked")
