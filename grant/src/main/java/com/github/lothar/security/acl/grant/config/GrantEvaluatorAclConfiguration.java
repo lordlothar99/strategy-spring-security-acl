fe37f45baeeee5c91a9df04c44494b1cbec701c3
François Lecomte <francois.lecomte@gmail.com>
2016-04-03 12:32:40 +0200

    GrantEvaluator feature #10 ; tests

--- grant/src/main/java/com/github/lothar/security/acl/grant/config/GrantEvaluatorAclConfiguration.java
+++ grant/src/main/java/com/github/lothar/security/acl/grant/config/GrantEvaluatorAclConfiguration.java
@@ -1,17 +1,15 @@
 /*******************************************************************************
  * Copyright 2002-2016 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *******************************************************************************/
 package com.github.lothar.security.acl.grant.config;
 
@@ -24,11 +22,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 import com.github.lothar.security.acl.AclStrategyProvider;
+import com.github.lothar.security.acl.SimpleAclStrategy;
 import com.github.lothar.security.acl.compound.AclComposersRegistry;
 import com.github.lothar.security.acl.config.AclConfiguration;
 import com.github.lothar.security.acl.grant.AclPermissionEvaluator;
+import com.github.lothar.security.acl.grant.GrantEvaluator;
 import com.github.lothar.security.acl.grant.GrantEvaluatorFeature;
 import com.github.lothar.security.acl.grant.compound.GrantEvaluatorComposer;
+import com.github.lothar.security.acl.grant.evaluators.AllowAllGrantEvaluator;
+import com.github.lothar.security.acl.grant.evaluators.DenyAllGrantEvaluator;
 
 @Configuration
 @Import(AclConfiguration.class)
@@ -46,8 +48,7 @@ public class GrantEvaluatorAclConfiguration {
 
   @Bean
   @ConditionalOnMissingBean(GrantEvaluatorComposer.class)
-  public GrantEvaluatorComposer grantEvaluatorComposer(
-      AclComposersRegistry registry) {
+  public GrantEvaluatorComposer grantEvaluatorComposer(AclComposersRegistry registry) {
     GrantEvaluatorComposer composer = new GrantEvaluatorComposer();
     registry.register(grantEvaluatorFeature, composer);
     return composer;
@@ -57,4 +58,19 @@ public class GrantEvaluatorAclConfiguration {
   public AclPermissionEvaluator aclPermissionEvaluator(AclStrategyProvider strategyProvider) {
     return new AclPermissionEvaluator(strategyProvider, grantEvaluatorFeature);
   }
+
+  @Bean
+  public GrantEvaluator allowAllGrantEvaluator(SimpleAclStrategy allowAllStrategy) {
+    GrantEvaluator allowAllGrantEvaluator = new AllowAllGrantEvaluator();
+    allowAllStrategy.install(grantEvaluatorFeature, allowAllGrantEvaluator);
+    return allowAllGrantEvaluator;
+  }
+
+  @Bean
+  public GrantEvaluator denyAllGrantEvaluator(SimpleAclStrategy denyAllStrategy) {
+    GrantEvaluator denyAllGrantEvaluator = new DenyAllGrantEvaluator();
+    denyAllStrategy.install(grantEvaluatorFeature, denyAllGrantEvaluator);
+    return denyAllGrantEvaluator;
+  }
+
 }
--- grant/src/test/java/com/github/lothar/security/acl/grant/GrantEvaluatorTest.java
+++ grant/src/test/java/com/github/lothar/security/acl/grant/GrantEvaluatorTest.java
@@ -1,17 +1,15 @@
 /*******************************************************************************
  * Copyright 2002-2016 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *******************************************************************************/
 package com.github.lothar.security.acl.grant;
 
@@ -19,13 +17,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import javax.annotation.Resource;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.security.access.PermissionEvaluator;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.github.lothar.security.acl.grant.config.GrantEvaluatorAclConfiguration;
+import com.github.lothar.security.acl.grant.domain.AllowedToAllDomainObject;
+import com.github.lothar.security.acl.grant.domain.DeniedToAllDomainObject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(GrantEvaluatorAclConfiguration.class)
@@ -33,10 +36,45 @@ public class GrantEvaluatorTest {
 
   @Resource
   private PermissionEvaluator permissionEvaluator;
+  private Authentication authentication;
+
+  @Before
+  public void init() {
+    authentication = new TestingAuthenticationToken("fake principal", "fake credentials");
+  }
 
   @Test
   public void should_permissionEvaluator_be_loaded() {
     assertThat(permissionEvaluator).isInstanceOf(AclPermissionEvaluator.class);
   }
 
+  @Test
+  public void should_grant_when_allowAllStrategy_loaded() {
+    Object domainObject = new AllowedToAllDomainObject();
+    boolean grant =
+        permissionEvaluator.hasPermission(authentication, domainObject, "fake permission");
+    assertThat(grant).isTrue();
+  }
+
+  @Test
+  public void should_grant_when_allowAllStrategy_loaded_using_weak_ref_signature() {
+    boolean grant = permissionEvaluator.hasPermission(authentication, "fake id",
+        AllowedToAllDomainObject.class.getName(), "fake permission");
+    assertThat(grant).isTrue();
+  }
+
+  @Test
+  public void should_not_grant_when_denyAllStrategy_loaded() {
+    Object domainObject = new DeniedToAllDomainObject();
+    boolean grant =
+        permissionEvaluator.hasPermission(authentication, domainObject, "fake permission");
+    assertThat(grant).isFalse();
+  }
+
+  @Test
+  public void should_not_grant_when_denyAllStrategy_loaded_using_weak_ref_signature() {
+    boolean grant = permissionEvaluator.hasPermission(authentication, "fake id",
+        DeniedToAllDomainObject.class.getName(), "fake permission");
+    assertThat(grant).isFalse();
+  }
 }
