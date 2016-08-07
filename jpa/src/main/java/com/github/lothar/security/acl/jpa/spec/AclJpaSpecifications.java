cceee8e02bed7bb3e94c1f0179d335c9cce5c502
François Lecomte <francois.lecomte@gmail.com>
2016-08-07 10:26:16 +0200

    collection method is public

--- jpa/src/main/java/com/github/lothar/security/acl/jpa/spec/AclJpaSpecifications.java
+++ jpa/src/main/java/com/github/lothar/security/acl/jpa/spec/AclJpaSpecifications.java
@@ -35,7 +35,7 @@ public class AclJpaSpecifications {
     return new BiFunctionSpecification<>((root, cb) -> cb.equal(root.get("id"), id));
   }
 
-  private static <T> Collection<T> collection(Iterable<T> iterable) {
+  public static <T> Collection<T> collection(Iterable<T> iterable) {
     if (iterable instanceof Collection) {
       return (Collection<T>) iterable;
     } else {
